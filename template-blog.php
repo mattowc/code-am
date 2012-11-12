ede7c89acc1d1a160cb17e28891cc432169f8f10
Jon McDonald <jon@onewebcentric.com>
2012-11-12 15:33:12 -0700

    Press / Featured Posts
    * Adds ability for featured posts
    * Adds press logos to bottom
    * Brand new press page
    * Brand new ABC press page

--- template-blog.php
+++ template-blog.php
@@ -14,9 +14,52 @@ get_header('blog');
 $args       = array('type'=>'post');
 $categories = get_categories( $args );
 
+// First show the featured category
+$args = array(
+		'type'=>'post', 
+		'category_name'=>'featured', 
+		'posts_per_page'=>'3');
+query_posts($args);
+
+$count = 0;
+$featured = get_category( 58 );
+
+// Prepare a separate for this div
+echo'<div style="clear: both;">';
+echo'<h3 class="jm-cat"><a href="' . get_category_link($featured->term_id) . '">Featured</a></h3>';
+
+// Loop through the posts, display properly
+while(have_posts()): the_post(); ?>
+	<div class="one_third <?php if($count == 0): echo 'first'; elseif($count==2): echo 'last'; endif;?>">
+		<a href="<?php the_permalink(); ?>">
+			<?php if(has_post_thumbnail()): ?>
+				<?php the_post_thumbnail(array(300, 168), array('class'=>'jm-thumb')); ?>
+			<?php else: ?>
+				<img class="wp-post-image jm-thumb" src="<?php bloginfo('url') ?>/wp-content/uploads/2012/10/AmberMay.jpg" />
+			<?php endif; ?>
+		</a>
+		<h4>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h4>
+		<div class="the_story">
+				<?php the_excerpt(); ?>
+		</div>
+	</div>
+<?php $count++; endwhile; // End post loop, and increment the counter 
+wp_reset_query();
+echo'<div class="news-feed-divider"><a href="' . 
+get_category_link($featured->term_id) . '" class="button small accent1   alignright">More from ' . 
+$featured->name . '</a></div>';
+echo'</div>';
+
 // Loop through the categories, and extract the three most recent posts
 foreach($categories as $category)
 {
+	if( $category->slug == 'featured' )
+		continue;
+
 	// Prepare a separate for this div
 	echo'<div style="clear: both;">';
 	echo'<h3 class="jm-cat"><a href="' . get_category_link($category->term_id) . '">' . 
